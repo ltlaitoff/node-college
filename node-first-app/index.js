ba195ab03bb7d06adab74569abf40f34132da446
Ivan Baluta <ltlaitoff@gmail.com>
2022-10-17 17:22:27 +0300

    feat: Connected MongoDB

--- node-first-app/index.js
+++ node-first-app/index.js
@@ -1,6 +1,16 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const { DEFAULT_PORT } = require('./config')
 const homeRouter = require('./routes/home')
+
+mongoose.connect('mongodb://127.0.0.1:27017/db_users', function (err) {
+	if (err) {
+		console.error('[MongoDB]: Error')
+		throw err
+	}
+
+	console.log('[MongoDB]: Successfully connected')
+})
 
 const PORT = process.env.PORT || DEFAULT_PORT
 const app = express()
--- node-first-app/routes/home.js
+++ node-first-app/routes/home.js
@@ -1,23 +1,33 @@
 const { Router } = require('express')
+const UserModel = require('../models/UserModel')
+
 const router = Router()
 
-const USERS = [
-	{
-		name: 'Masha',
-		age: 19,
-	},
-	{
-		name: " 'Andrii",
-		age: 20,
-	},
-]
+// const USERS = UserModel.find((err, users) => {
+// 	if (err) {
+// 		console.error('[MongoDB]: Error in routes/home')
+// 		throw err
+// 	}
 
-router.get('/', function (request, response) {
-	response.json(USERS)
+// 	return users
+// })
+
+// console.log(USERS)
+
+router.get('/', (request, response) => {
+	UserModel.find((err, users) => {
+		if (err) {
+			response.send(err)
+		}
+
+		console.log(users)
+
+		response.json(users)
+	})
 })
 
-router.post('/', function (request, response) {
-	response.send(USERS)
-})
+// router.post('/', function (request, response) {
+// 	response.send(USERS)
+// })
 
 module.exports = router
